f7c9a964891f91836a031ef73bfc9f8741bfa83c
Kurt Hornik <Kurt.Hornik@R-project.org>
2010-08-05 00:00:00 +0000

    version 0.0-8

--- R/language.R
+++ R/language.R
@@ -1,5 +1,5 @@
 parse_IETF_language_tag <-
-function(x)
+function(x, expand = FALSE)
 {
     ## No support for grandfathered tags for now.
     ## Hence, assume the form is
@@ -41,7 +41,6 @@ function(x)
             else
                 stop(gettextf("Invalid subtag: %s", p))
             i <- i + 1L
-            cat(i, "\n")
         }
         ## If there are leftovers ...
         if(i <= n)
@@ -49,10 +48,49 @@ function(x)
         out
     }
 
-    do.call(rbind,
-            lapply(strsplit(x, "-", fixed = TRUE), process_chunks))
+    y <- do.call(rbind,
+                 lapply(strsplit(x, "-", fixed = TRUE),
+                        process_chunks))
+    if(!expand)
+        return(y)
+
+    pos <- match(sprintf("%s=%s",
+                         rep(tolower(colnames(y)), each = nrow(y)),
+                         y),
+                 IANA_language_subtag_registry$Index)
+    ind <- !is.na(y)
+    ## Note that descriptions can have more than one element, so give a
+    ## character frame ...
+    z <- rep.int(list(character()), length(y))
+    z[ind] <- IANA_language_subtag_registry$Description[pos][ind]
+    z <- as.data.frame(matrix(z, nrow(y)))
+    dimnames(z) <- list(x, colnames(y))
+    z
 }
 
 ## Not sure what to do with all grandfathered langtag from the IANA
 ## registry.  Some have a preferred language value ... but the rest?
+
+get_IANA_language_subtag_registry <-
+function(con = "http://www.iana.org/assignments/language-subtag-registry")
+{
+    ## This is a collection of records in tag-value format, but
+    ## unfortunately separated by '%%' lines rather than empty lines, so
+    ## we cannot use read.dcf() directly.  Let us keep things simple:
+    ## extract the records, write them out as DCF, and call read.dcf().
+
+    lines <- readLines(con)
+    pos <- grep("^%%", lines)
+    ## The first chunk is something like
+    ##   File-Date: 2009-03-13
+    ## which we drop.
+    lines[c(seq_len(pos[1L]), pos[-1L])] <- ""
+    tcon <- textConnection(lines, encoding = "UTF-8")
+    on.exit(close(tcon))
+    db <- read.dcf(tcon, all = TRUE)
+    ## Add index for lookups.
+    db$Index <- sprintf("%s=%s", db$Type, db$Subtag)
+    db$Type <- factor(db$Type)
+    db
+}
 
