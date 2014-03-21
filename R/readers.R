f8e09f3b70d916c6a1cc59dc542eaceb790b7a00
Kurt Hornik <Kurt.Hornik@R-project.org>
2014-03-21 00:00:00 +0000

    version 0.0-17

--- R/readers.R
+++ R/readers.R
@@ -1,7 +1,7 @@
 readBytes <-
 function(con)
 {
-    s <- rawToChar(readBin(con, raw(), .Machine$integer.max))
+    s <- rawToChar(read_all_bytes(con))
     Encoding(s) <- "bytes"
     s
 }
@@ -9,6 +9,28 @@ function(con)
 readChars <-
 function(con, encoding = "")    
 {
-    s <- rawToChar(readBin(con, raw(), .Machine$integer.max))
+    s <- rawToChar(read_all_bytes(con))    
     iconv(s, from = encoding, to = "UTF-8", sub = "byte")
+}
+
+read_all_bytes <-
+function(con, chunksize = 2 ^ 16)
+{
+    if(is.character(con)) {
+        return(readBin(con, raw(), file.info(con)$size))
+    }
+
+    if(!isOpen(con)) {
+        open(con, "rb")
+        on.exit(close(con))
+    }
+
+    bytes <- list()
+    repeat {
+        chunk <- readBin(con, raw(), chunksize)
+        bytes <- c(bytes, list(chunk))
+        if(length(chunk) < chunksize) break
+    }
+
+    unlist(bytes)
 }
